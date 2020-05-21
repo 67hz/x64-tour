b22df1fe2625554f793bf9b855e050b7ec12393e
Aaron Hinojosa <67hz@protonmail.com>
2020-05-21 18:59:12 -0500

    simplified printer

--- BitOperations/printc.asm
+++ BitOperations/printc.asm
@@ -14,46 +14,35 @@ section .text
 printc:
 push    rbp
 mov     rbp, rsp
-        push        rbx     ; callee-saved reg
         push        r12     ; callee-saved reg
-        xor         rbx, rbx
         xor         r12, r12
         mov         rcx, [num_bits]
         mov         r12, rdi    ; r12 storage of value
 .bloop:
+        dec         rcx
         xor         rax, rax
-        mov         rax, r12    ; copy target value into rax
-        sar         rax, cl-1     ; must be 8 byte reg
-        jz          .isZero  ; num & 1 == 0
-        jnz         .notZero
+        mov         rax, r12    ; copy target value into rax;
+        sar         rax, cl     ; must be 8 byte reg
+        and         rax, 1
 
-
-        
-.loopdeaux:
         mov         rdi, fmt1
-        mov         rsi, rbx
+        mov         rsi, rax
         mov         rax, 0      ; no xmm0 registers
 ; printf steps on rcx so preserve or 
         push        rcx
         call        printf      ; @TODO call printf after processing all bits
         pop         rcx         ; fails because rcx pops when 0 loads a garbage value
-        loop        .bloop
 
-        ; dec         rcx
-        ; test        rcx,rcx     ; fastest is it a zero test
-        ; jgz         .bloop
+        test        rcx,rcx     ; fastest is it a zero test
+        jnz         .bloop
 
 
-.notZero:
-    mov         rbx, 1
-    jmp         .loopdeaux
 
-.isZero:
-    mov         rbx, 0
-    jmp         .loopdeaux
-        
+
 pop     r12
-pop     rbx
+
+        
+
 
 mov     rsp, rbp
 pop     rbp
