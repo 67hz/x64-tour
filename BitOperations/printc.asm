b1ea49c74c88c75ffe2902f3178a1dd9e3a237b4
Aaron Hinojosa <67hz@protonmail.com>
2020-05-21 18:43:37 -0500

    pre ANDing simplified

--- BitOperations/printc.asm
+++ BitOperations/printc.asm
@@ -19,31 +19,38 @@ mov     rbp, rsp
         xor         rbx, rbx
         xor         r12, r12
         mov         rcx, [num_bits]
-        mov         r12, rdi    ; lt storage of value
-bloop:
-        push rcx
+        mov         r12, rdi    ; r12 storage of value
+.bloop:
         xor         rax, rax
         mov         rax, r12    ; copy target value into rax
-        sar         rax, cl
-        test        rax, 1  ; AND compare rax with 1
-        jz          isZero  ; num & 1 == 0
+        sar         rax, cl-1     ; must be 8 byte reg
+        jz          .isZero  ; num & 1 == 0
+        jnz         .notZero
 
-        mov         rbx, 1
-        jmp         loopdeaux
 
         
-loopdeaux:
+.loopdeaux:
         mov         rdi, fmt1
         mov         rsi, rbx
         mov         rax, 0      ; no xmm0 registers
 ; printf steps on rcx so preserve or 
+        push        rcx
         call        printf      ; @TODO call printf after processing all bits
-        pop         rcx
-        loop        bloop
+        pop         rcx         ; fails because rcx pops when 0 loads a garbage value
+        loop        .bloop
 
-isZero:
+        ; dec         rcx
+        ; test        rcx,rcx     ; fastest is it a zero test
+        ; jgz         .bloop
+
+
+.notZero:
+    mov         rbx, 1
+    jmp         .loopdeaux
+
+.isZero:
     mov         rbx, 0
-    loop        loopdeaux
+    jmp         .loopdeaux
         
 pop     r12
 pop     rbx
