ab7303e0c24b33d51f0b71c524036120e1affa4c
Aaron Hinojosa <67hz@protonmail.com>
2020-08-13 19:49:18 -0500

    mem addressing test

---
+++ LLP/mappings_loop.asm
@@ -0,0 +1,7 @@
+section .data
+	correct: 	dq -1
+section code
+global _start
+_start:
+jmp _start
+
--- LLP/print_rax.asm
+++ LLP/print_rax.asm
@@ -1,38 +1,77 @@
 section .data
 codes:
 	db	'0123456789ABCDEF'
+NL: 
+	db 	0xa
 
 section .text
 global _start
-_start:
-	; number 1122... in hex format
-	mov 	rax, 0x1122334455667788
 
+print_newline:
+	push 	rbp
+	mov 	rbp, rsp
+
+	mov 	rax, 1
+	mov 	rdi, 1
+	mov 	rsi, NL
+	mov 	rdx, 1
+
+	syscall
+
+	mov 	rsp, rbp
+	pop 	rbp
+	ret
+
+; each 4 bits should be ouptut as one hex digit
+; shift and AND to isolate them
+; result is offset in 'codes' array
+
+print_hex:
+	push 	rbp
+	mov 	rbp, rsp
 	mov 	rdi, 1
 	mov 	rdx, 1
 	mov 	rcx, 64
-	; each 4 bits should be ouptut as one hex digit
-	; shift and AND to isolate them
-	; result is offset in 'codes' array
-.loop:
-	push 	rax
-	sub 	rcx, 4
+	mov 	rax, 0x1122334455667788
 
-	sar 	rax, cl
-	and 	rax, 0xf
+	iterate:
+		push 	rax
+		sub 	rcx, 4
 
-	lea 	rsi, [codes + rax]
-	mov 	rax, 1
+		sar 	rax, cl
+		and 	rax, 0xf
 
-	; sycall clobbers rcx and r11
-	push 	rcx
-	syscall
-	pop 	rcx
-	pop 	rax
+		lea 	rsi, [codes + rax]
+		mov 	rax, 1
 
-	test 	rcx, rcx
-	jnz 	.loop
+		; sycall clobbers rcx and r11
+		push 	rcx
+		syscall
+		pop 	rcx
+		pop 	rax
+
+		test 	rcx, rcx
+		jnz 	iterate
+
+
+	mov 	rsp, rbp
+	pop 	rbp
+	ret
+
+
+_start:
+	push 	rbp
+	mov    	rbp, rsp
+
+	call print_hex
+
+
+	call print_newline
+	call print_newline
+
 
 	mov 	rax, 60 	; exit syscall
 	xor 	rdi, rdi
 	syscall
+
+
